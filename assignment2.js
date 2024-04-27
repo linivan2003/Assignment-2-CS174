5d57bec4db3fc2e26bbbfcdaceb60f608bb98d72
Ivan Lin <149336992+linivan2003@users.noreply.github.com>
2024-04-26 18:21:08 -0700

    stacked blue boxes and draw_box function

--- assignment2.js
+++ assignment2.js
@@ -112,7 +112,9 @@ export class Assignment2 extends Base_Scene {
         // TODO:  Helper function for requirement 3 (see hint).
         //        This should make changes to the model_transform matrix, draw the next box, and return the newest model_transform.
         // Hint:  You can add more parameters for this function, like the desired color, index of the box, etc.
-
+        const blue = hex_color("#1a9ffa");
+        this.shapes.cube.draw(context, program_state, model_transform, this.materials.plastic.override({color:blue}));
+        model_transform = model_transform.times(Mat4.translation(0, 2, 0));
         return model_transform;
     }
 
@@ -121,8 +123,10 @@ export class Assignment2 extends Base_Scene {
         const blue = hex_color("#1a9ffa");
         let model_transform = Mat4.identity();
 
-        // Example for drawing a cube, you can remove this line if needed
-        this.shapes.cube.draw(context, program_state, model_transform, this.materials.plastic.override({color:blue}));
+      
         // TODO:  Draw your entire scene here.  Use this.draw_box( graphics_state, model_transform ) to call your helper.
+        for (let i = 0; i <= 8; i++) {
+            model_transform = this.draw_box(context,program_state,model_transform);
+        }
     }
 }
